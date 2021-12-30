112bc4e1c5760338b9394c6780841e8dc93b6b13
touza <touzaelhassan@gmail.com>
2021-12-30 14:06:56 +0100

    start menu page

--- js/lunch.js
+++
@@ -1,8 +0,0 @@
-const hamburgerMenu = document.querySelector('.mobile__menu');
-const headerNavigation = document.querySelector('.header__navigation');
-const overlay = document.querySelector('.overlay');
-
-hamburgerMenu.addEventListener('click', function () {
-  headerNavigation.classList.toggle('active');
-  overlay.classList.toggle('overlay--active');
-});
