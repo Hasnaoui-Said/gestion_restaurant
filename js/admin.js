a35a9f963fe03284f6ab480b8af2b99e4c64428b
hasnaoui-said <saidhasnaoui.uce@gmail.com>
2022-01-07 00:02:12 +0100

    update file dashboard and plat commandee

--- js/admin.js
+++ js/admin.js
@@ -13,6 +13,8 @@ function menu__toogle(){
     nav__bar.classList.toggle('active');
     let main__body = document.querySelector('.main__body');
     main__body.classList.toggle('active');
+    let container = document.querySelector('.container');
+    container.classList.toggle('active');
   };
   function puls__down(){
     let plus__down = document.querySelector('.plus__down');
